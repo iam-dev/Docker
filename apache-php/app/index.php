db5ae0a340e51eab85ff5680dc2fd93ed86bbd02
IaM DEV <30754770+iam-dev@users.noreply.github.com>
2018-03-15 14:18:57 +0100

    Add apache-php

---
+++ apache-php/app/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Dockerfile example for Apache 2 on Ubuntu 17.10</title>
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="vendor/bootstrap/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header></header>
+    <main>
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Default page</h1>
+                <p class="lead text-muted">Dockerfile example for Apache 2 on Ubuntu 17.10.
+                </p>
+                <p>
+                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
+                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                </p>
+            </div>
+        </section>
+        <?php phpinfo(); ?>
+    </main>
+    <footer></footer>
+
+<script src="vendor/jquery/3.3.1/jquery.min.js"></script>
+</body>
+</html>
